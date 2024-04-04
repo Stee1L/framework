8a798bd3f22d7ed02a18eb446a6f4f5e23df2142
mac <superstart8016@gmail.com>
2024-04-05 02:30:11 +0700

    Улучшение регистрации

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Src\Route;
 
 Route::add('cats', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
-Route::add('', [Controller\Site::class, 'startPage']);
 Route::add('signup', [Controller\Site::class, 'signup']);
+Route::add('login', [Controller\Site::class, 'login']);
+Route::add('logout', [Controller\Site::class, 'logout']);
 
 
