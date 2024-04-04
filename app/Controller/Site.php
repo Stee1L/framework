fa2beafe04d69f00dcd06846c416f42719f6a591
mac <superstart8016@gmail.com>
2024-04-05 01:22:58 +0700

    Добавление регистрации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,17 +5,32 @@ namespace Controller;
 use Illuminate\Support\Facades\DB;
 use Model\Cat;
 use Src\View;
+use Src\Request;
+use Model\User;
+
 
 class Site
 {
-    public function index(): string
-    {
-        $posts = Cat::all();
-        return (new View())->render('site.post', ['posts' => $posts]);
-    }
+
 
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function index(Request $request): string
+    {
+        $posts = !is_null($request->id) ? Cat::where('id', $request->id)->get() : Cat::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Src\Route;
 Route::add('cats', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
 Route::add('', [Controller\Site::class, 'startPage']);
+Route::add('signup', [Controller\Site::class, 'signup']);
+
 
